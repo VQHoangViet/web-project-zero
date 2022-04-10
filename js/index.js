081b8566eb2743fbbb86b2738a10cb5d7ca98dd5
nateclint <giahungnguyen999@gmail.com>
2022-04-10 15:09:27 +0700

    header without onHover

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@
 $(document).ready(function () {
     $('#nav').load('../components/nav.html')
     $('#footer').load('../components/footer.html')
+    $.get('../components/import.html', function(import_string) {
+        $('head').append(import_string)
+    })
+    scroll(0, 0)
 })
 
 // endregion
